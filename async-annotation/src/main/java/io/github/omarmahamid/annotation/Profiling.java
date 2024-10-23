42dff8925247063f439de851da1ee17ba3d2f6f7
omar.mahamid <omar.taiseerr@gmail.com>
2024-10-23 22:12:18 +0300

    update pom for annotation

---
+++ async-annotation/src/main/java/io/github/omarmahamid/annotation/Profiling.java
@@ -0,0 +1,11 @@
+package io.github.omarmahamid.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Profiling {
+
+
+}
