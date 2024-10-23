1bf33544c229448a8fb2a225859dd7fa5bccf55e
omar.mahamid <omar.taiseerr@gmail.com>
2024-10-24 00:01:10 +0300

    add aspect4j for profiling with annotation

---
+++ async-annotation/src/main/java/io/github/omarmahamid/annotation/Main.java
@@ -0,0 +1,19 @@
+package io.github.omarmahamid.annotation;
+
+
+import java.math.BigDecimal;
+
+@Profiling
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        BigDecimal decimal = new BigDecimal("1.23");
+        while (true){
+            decimal.abs();
+            decimal.add(new BigDecimal("1.23"));
+        }
+
+    }
+}
